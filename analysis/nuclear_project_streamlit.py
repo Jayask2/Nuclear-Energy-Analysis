d092ca38dcca773329acdc844effb4279da6dd75
Iasonas Askounis <iaskounis@yahoo.com>
2025-02-01 14:01:37 +0200

    Update nuclear_project_streamlit.py

--- analysis/nuclear_project_streamlit.py
+++ analysis/nuclear_project_streamlit.py
@@ -1,8 +1,6 @@
+pip install plotly
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import json
-import seaborn as sns
 import streamlit as st
 import plotly.express as px
 import folium 
